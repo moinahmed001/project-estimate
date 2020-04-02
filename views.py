1e479c78729bc0501041ffb467fa44ab0c4d4e41
Ahmed, Moin <Moin.Ahmed@sky.uk>
2020-04-02 22:41:54 +0100

    db commit

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views_routes = Blueprint('views_routes', __name__)
 
 @views_routes.route('/test')
 def test():
     return 'it works!'
+
+@views_routes.route('/test1')
+def index():
+    return render_template("index.html")
