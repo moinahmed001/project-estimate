cc38f64b5c05c595ca44be7fbe99c73d76094a2e
Ahmed, Moin <Moin.Ahmed@sky.uk>
2020-04-24 12:34:26 +0100

    Added config diff tool for web

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from flask import Flask, escape, request, Blueprint, redirect, url_for
+from flask import Flask, escape, request, Blueprint, redirect, url_for, render_template
 from views import views_routes
 from ingestApi import ingest_api_routes
 import epicsModel, issuesModel, projectEpicsAndTicketsModel as peatm, projectsModel, epicsIssuesModel, ticketsModel, userProjectAccessModel, reportingModel, epicsIssuesRemovedModel, contingencyModel, boardsModel
 from flask import jsonify
+from contextlib import closing
+from requests.exceptions import RequestException
+from requests import get
+from deepdiff import DeepDiff
+import json, re
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -181,3 +187,136 @@ def api_epic_issues(board_name, epic_number):
 @app.route('/api/epicIssuesFullDetails/<board_name>/<epic_number>')
 def api_epic_issues_full_details(board_name, epic_number):
     return epicsIssuesModel.get_epic_issues_full_details(board_name, epic_number)
+
+
+# config server
+@app.route("/web/config/<territory>/<env>")
+def web_config_check(territory, env):
+    config_version_from = request.args.get('config_version_from', default = '4cf564f', type = str)
+    config_version_to = request.args.get('config_version_to', default = 'aa1dfa9', type = str)
+    if territory == 'es':
+        if env == 'prod':
+            full_url = 'https://www.sky.es/international/static/{0}/config/es/nowtv/nowtv/web/production/config.json'
+        elif env == 'func':
+            full_url = 'https://www.func.prod.sky.es/international/static/{0}/config/es/nowtv/nowtv/web/func-prod/config.json'
+    elif territory == 'gb':
+        if env == 'prod':
+            full_url = 'https://www.nowtv.com/international/static/{0}/config/gb/nowtv/nowtv/web/production/config.json'
+        elif env == 'func':
+            full_url = 'https://www.func.prod.nowtv.com/international/static/{0}/config/gb/nowtv/nowtv/web/func-prod/config.json'
+    elif territory == 'ie':
+        if env == 'prod':
+            full_url = 'https://www.nowtv.com/international/static/{0}/config/ie/nowtv/nowtv/web/production/config.json'
+        elif env == 'func':
+            full_url = 'https://www.func.prod.nowtv.com/international/static/{0}/config/ie/nowtv/nowtv/web/func-prod/config.json'
+    elif territory == 'it':
+        if env == 'prod':
+            full_url = 'https://www.nowtv.it/international/static/{0}/config/it/nowtv/nowtv/web/production/config.json'
+        elif env == 'func':
+            full_url = 'https://c3.nowtv.it/international/static/{0}/config/it/nowtv/nowtv/web/func-prod/config.json'
+    elif territory == 'de':
+        if env == 'prod':
+            full_url = 'https://skyticket.sky.de/international/static/{0}/config/de/nowtv/nowtv/web/production/config.json'
+        elif env == 'func':
+            full_url = 'https://test.skyticket.sky.de/international/static/{0}/config/de/nowtv/nowtv/web/func-prod/config.json'
+
+
+
+    url = full_url.format(config_version_from)
+    url_to_compare = full_url.format(config_version_to)
+
+    old_url_fetched_response = simple_get_json(url).decode('utf-8')
+    new_url_fetched_response = simple_get_json(url_to_compare).decode('utf-8')
+
+    diff_result = DeepDiff(json.loads(old_url_fetched_response), json.loads(new_url_fetched_response), ignore_order=True)
+
+    try:
+        return render_template("config_diff_web.html", live_config_hash = all_config_hash(env), diff_result = diff_result, territory=territory, config_version_from=config_version_from, config_version_to=config_version_to, env=env, url=url, url_to_compare=url_to_compare)
+    except Exception as e:
+        return str(e)
+
+@app.route("/web/config/<env>/hash")
+def web_config_hash(env):
+    return jsonify(all_config_hash(env))
+
+
+def all_config_hash(env):
+    live_config_hash = {}
+    if env == 'prod':
+        all_urls = [
+            "https://www.nowtv.com/gb/watch/home", "https://www.nowtv.com/ie/watch/home", "https://skyticket.sky.de/watch/home", "https://www.nowtv.it/watch/home", "https://www.sky.es/ver/inicio"
+        ]
+    else:
+        all_urls = [
+            "https://www.func.prod.nowtv.com/gb/watch/home", "https://test.skyticket.sky.de/watch/home", "https://c3.nowtv.it/watch/home", "https://www.func.prod.sky.es/ver/inicio"
+        ]
+        # "https://func.prod.nowtv.com/ie/watch/home",
+
+    for url in all_urls:
+        if url == "https://test.skyticket.sky.de/watch/home":
+            raw_html = simple_get(url, auth=('StormSkyDE', '1Jky73spyy!'))
+        elif url == "https://c3.nowtv.it/watch/home":
+            raw_html = simple_get(url, auth=('river', 'PWD!sky0nl1n3#2014'))
+        else:
+            raw_html = simple_get(url)
+
+        if raw_html != None:
+            html = BeautifulSoup(raw_html, 'html.parser')
+            string_html = raw_html.decode("utf-8").split()
+            release_hash = 'UNKNOWN'
+
+
+            for aString in string_html:
+                if "href" in aString:
+                    hash = re.findall(r'static\/(.*?)\/core', aString)
+                    if len(hash) == 1:
+                        release_hash = hash[0]
+                        live_config_hash[url] = hash[0]
+                        break
+
+    return live_config_hash
+
+
+def simple_get_json(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        print('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def simple_get(url, auth=()):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True, auth=auth)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        print('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None)
